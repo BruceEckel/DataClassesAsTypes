e2004c3983d98eb7822942d22ca9ff45e41f7cd1
Bruce Eckel <mindviewinc@gmail.com>
2022-02-09 10:39:38 -0700

    Better pattern matching + incorrect bug report

--- 6_BirthDate.py
+++ 6_BirthDate.py
@@ -4,7 +4,7 @@ from check import check
 @dataclass(frozen=True)
 class Day:
     day: int
-    def __str__(self):
+    def __repr__(self):
         return f"Day({self.day})"
     def __post_init__(self) -> None:
         check(0 < self.day <= 31, f"{self.day}: day of month out of range")
@@ -12,7 +12,7 @@ class Day:
 @dataclass(frozen=True)
 class Month:
     month: int
-    def __str__(self):
+    def __repr__(self):
         return f"Month({self.month})"
     def __post_init__(self) -> None:
         check(1 <= self.month <= 12, f"{self.month}: month out of range")
@@ -21,7 +21,7 @@ class Month:
 class MonthDay:
     month: Month
     day: Day
-    def __str__(self):
+    def __repr__(self):
         return f"MonthDay({self.month}, {self.day})"
     def validate(self, max_day):
         check(self.day.day <= max_day,
@@ -32,14 +32,13 @@ class MonthDay:
         All the rest have 31
         Except February, which stands alone with 28
         """
-        print(f"self.month.month: {self.month.month}")
-        print(f"self.day.day: {self.day.day}")
-        match self.month.month:
-            case 2:
-                print("case 2")
+        print(self)
+        match self.month:
+            case Month(2):
+                print("case Month(2)")
                 self.validate(28)
-            case 9 | 4 | 6 | 11:
-                print("case 9 | 4 | 6 | 11")
+            case Month(9 | 4 | 6 | 11):
+                print("case Month(9 | 4 | 6 | 11)")
                 self.validate(30)
             case _:
                 print("case _")
@@ -48,7 +47,7 @@ class MonthDay:
 @dataclass(frozen=True)
 class Year:
     year: int
-    def __str__(self):
+    def __repr__(self):
         return f"Year({self.year})"
     def __post_init__(self) -> None:
         check(1900 < self.year <= 2022, f"{self.year}: year out of range")
@@ -58,11 +57,14 @@ class BirthDate:
     m: Month
     d: Day
     y: Year
-    def __str__(self):
-        return f"BirthDate({self.m}, {self.d}, {self.y})"
 
 if __name__ == '__main__':
     print(BirthDate(Month(7), Day(8), Year(1957)))
     print(BirthDate(Month(0), Day(32), Year(1857)))
-    print(MonthDay(Month(2), Day(31)))
-    print(MonthDay(Month(9), Day(31)))
+    MonthDay(Month(2), Day(31))
+    MonthDay(Month(9), Day(31))
+    MonthDay(Month(4), Day(31))
+    MonthDay(Month(6), Day(31))
+    MonthDay(Month(11), Day(31))
+    MonthDay(Month(12), Day(31))
+
--- Notes.md
+++ Notes.md
@@ -1,4 +1,3 @@
 # Notes
 - See if there's any other way to make an object of a frozen dataclass
 - Experiment with nesting classes: Nesting Day inside Month 
-- Or make a DayMonth class composed of Day and Month and check Day validity
