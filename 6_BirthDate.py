f42570b164ee17862157c9007d1fa76f1a07839c
Bruce Eckel <mindviewinc@gmail.com>
2022-02-09 23:14:58 -0700

    Update 6_BirthDate.py

--- 6_BirthDate.py
+++ 6_BirthDate.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from enum import Enum
 from check import check
 
 def trace(self): print(f"{self} __post_init__")
@@ -20,6 +21,20 @@ class Month:
     def __post_init__(self) -> None:
         trace(self)
         check(1 <= self.month <= 12, f"{self.month}: month out of range")
+
+class Month2(Enum):
+    JANUARY = (1, 31)
+    FEBRUARY = (2, 28)
+    MARCH = (3, 31)
+    APRIL = (4, 30)
+    MAY = (5, 31)
+    JUNE = (6, 30)
+    JULY = (7, 31)
+    AUGUST = (8, 31)
+    SEPTEMBER = (9, 30)
+    OCTOBER = (10, 31)
+    NOVEMBER = (11, 30)
+    DECEMBER = (12, 31)
 
 @dataclass(frozen=True)
 class Year:
